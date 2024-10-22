91c4d5a513f3eb1d3b2718c43a076d13243d7174
Tae-woong Kim <futureplanning@gopizza.kr>
2024-10-22 13:30:48 +0900

    Update responsive.tsx

--- styles/responsive.tsx
+++ styles/responsive.tsx
@@ -5,9 +5,12 @@ const breakpoints = {
   xl: '1280px',
 } as const;
 
-export const mq = Object.keys(breakpoints).reduce((acc, label) => {
-  acc[label as keyof typeof breakpoints] = `@media (min-width: ${
-    breakpoints[label as keyof typeof breakpoints]
-  })`;
-  return acc;
-}, {} as Record<keyof typeof breakpoints, string>);
+export const mq = Object.keys(breakpoints).reduce(
+  (acc, label) => {
+    acc[label as keyof typeof breakpoints] = `@media (min-width: ${
+      breakpoints[label as keyof typeof breakpoints]
+    })`;
+    return acc;
+  },
+  {} as Record<keyof typeof breakpoints, string>
+);
